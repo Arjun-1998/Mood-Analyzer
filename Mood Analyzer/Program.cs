09b12bc15ef8b06ffde5f432a12852b8aa53ce15
Arjun-1998 <arjunjaiswal8065@gmail.com>
2021-12-22 22:21:52 +0530

    [Arjun] Add ; Added Happy Or Sad Mood

---
+++ Mood Analyzer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoodAnalyserProblems
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            Console.WriteLine("*MoodAnalyzer*");
+
+        }
+    }
+}
